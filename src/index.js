38cb2ab4d86026f08048231a3fa12844d0979b8f
Jaguar838 <oleg.yamgurov@gmail.com>
2021-05-25 06:16:16 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './scss/styles.scss';
+import colors from './templates-menu/menu.json';
+
+const refs = {
+    body: document.body,
+    startBtn: document.querySelector('.js_start'),
+    stopBtn: document.querySelector('.js_stop'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let colorChange = null;
+function onStartBtnClick(event) {
+    if (refs.startBtn.hasAttribute('isActive')) {
+        return;
+    }
+    colorChange = setInterval(() => {
+        refs.startBtn.setAttribute('isActive', true);
+        refs.body.style.backgroundColor =
+            colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+    console.log(colorChange);
+    refs.startBtn.disabled = true;
+}
+function onStopBtnClick(event) {
+    clearInterval(colorChange);
+    refs.startBtn.disabled = false;
+    refs.startBtn.removeAttribute('isActive');
+}
